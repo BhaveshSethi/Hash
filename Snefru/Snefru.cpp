26b9f2e79fc6c836ebda71fb78d0018a487dcc4e
BhaveshSethi <bhaveshsethi1611@gmail.com>
2016-04-09 21:41:05 +0530

    Limit

--- Snefru/Snefru.cpp
+++ Snefru/Snefru.cpp
@@ -2,6 +2,7 @@
 #include<snefru.h>
 #include<iostream.h>
 #include<conio.h>
+#include<iomanip.h>
 
 #define blockSize 512
 
@@ -12,12 +13,16 @@ int32 rotate(int32 x, int shift)
 	return (x>>shift | x<<(32-shift));
 }
 
-void E512(int32 res[16], int32 x[16], int passes)
+void E512(int32 x[16], int passes)
 {
 	int idx,byteInWord,i,next,last;      	//Assumption bytes in a word are 4
 	int32 SBE;
 	for(idx=0;idx<passes;idx++)
 	{
+		/*cout<<endl<<idx;
+		for(i=0;i<4;i++)
+			cout<<setw(8)<<hex<<" "<<(int32)x[i];
+		*/
 		for(byteInWord = 0; byteInWord<4; byteInWord++)
 		{
 			for(i=0; i<16; i++)
@@ -37,12 +42,12 @@ void E512(int32 res[16], int32 x[16], int passes)
 void hash512(int32 res[16], int32 x[16], int passes)
 {
 	int i;
-	memset(res,0,64);
 	int32 block[16];
 	for(i=0;i<16;i++)
 		block[i]=x[i];
-	E512(res,x,passes);
-	for(i=0; i<16; i++)
+	memset(res,0,64);
+	E512(x,passes);
+	for(i=0; i<4; i++)
 		res[i] = block[i] ^ x[15-i];
 }
 void main()
@@ -57,5 +62,9 @@ void main()
 	hash512(op,ip,8);
 	for(i=0;i<4;i++)
 		cout<<hex<<(unsigned long int)op[i]<<" ";
+	hash512(ip,op,8);
+	cout<<"\n";
+	for(i=0;i<4;i++)
+		cout<<hex<<(unsigned long int)ip[i]<<" ";
 	getch();
 }

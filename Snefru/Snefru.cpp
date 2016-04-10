3c4a517b82a436e969348b0607590bf763c7358d
BhaveshSethi <bhaveshsethi1611@gmail.com>
2016-04-10 19:56:29 +0530

    Test

--- Snefru/Snefru.cpp
+++ Snefru/Snefru.cpp
@@ -3,6 +3,7 @@
 #include<iostream.h>
 #include<conio.h>
 #include<iomanip.h>
+#include<stdio.h>
 
 #define blockSize 512
 
@@ -51,20 +52,89 @@ void hash512(int32 res[16], int32 x[16], int passes)
 	for(i=0; i<4; i++)
 		res[i] = block[i] ^ x[15-i];
 }
+
+void increment(int32 bitLength[2], int increment)
+{
+	if(bitLength[1] + increment > 0xffffffffL)
+	{
+		bitLength[0]++;
+		bitLength[1] = increment + bitLength[1] - 0xffffffffL;
+	}
+	else
+		bitLength[1]+=increment;
+}
+
 void main()
 {
 	clrscr();
 //	cout<<sizeof(unsigned long int);
 //	int32 a = 5;
 //	cout<<rotate(a,32);
-	int32 ip[16],op[16];
-	int i;
+	int32 ip[16],op[16],bitLength[2] = {0,0};
+	char str[48],ch;
+	int i,flag=1;
+	FILE *xFile;
+	xFile = fopen("test.txt","rb");
+
+	for(i=0; i<4; i++)
+		op[i] = 0;
+	while(flag)
+	{
+		ch=fgetc(xFile);
+		i = 0;
+		while(ch!=EOF)
+		{
+			str[i++]=ch;
+			if(i==48)
+				break;
+			ch=fgetc(xFile);
+		}
+
+		increment(bitLength,8*i);
+
+		if(i==0)
+			break;
+		else if(i<48)
+		{
+			flag=0;
+			for(;i<48;i++)
+				str[i]=0;
+		}
+
+		for(i=0;i<4;i++)
+			ip[i] = op[i];
+		for(i=0;i<12;i++)
+			ip[i+4] = (str[4*i] << 24) |
+				  (str[4*i + 1] << 16) |
+				  (str[4*i + 2] << 8) |
+				   str[4*i + 3];
+		hash512(op,ip,8);
+		//for(i=0;i<4;i++)
+		//	cout<<op[i];
+	}
+	fclose(xFile);
+
+	for(i=0;i<16;i++)
+		ip[i] = op[i];
+
+	ip[14] = bitLength[0];
+	ip[15] = bitLength[1];
+
+	hash512(op,ip,8);
+
+	/*
 	memset(ip,0,64);
+	do
+	{
+		size = scanf;
+		cout<<endl<<str<<endl;
+	}while(size>0);
 	hash512(op,ip,8);
 	for(i=0;i<4;i++)
 		cout<<hex<<(unsigned long int)op[i]<<" ";
 	hash512(ip,op,8);
 	cout<<"\n";
+	*/
 	for(i=0;i<4;i++)
 		cout<<hex<<(unsigned long int)ip[i]<<" ";
 	getch();

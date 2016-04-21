eec3773c99793169ade28a9c91eb0b2987756bec
BhaveshSethi <bhaveshsethi1611@gmail.com>
2016-04-21 23:09:07 +0530

    Test Commit

--- update.bat
+++ update.bat
@@ -1,6 +1,8 @@
 cp ../Snefru.cpp		Snefru/Snefru.cpp
+cp ../Snefru_F.cpp		Snefru/Snefru_F.cpp
 cp ../MD4.cpp			MD4.cpp
-cp ../MD5.cpp			MD5.cpp
+cp ../MD5.cpp			MD5/MD5.cpp
+cp ../MD5_File.cpp		MD5/MD5_File.cpp
 
 cp ../../INCLUDE/Snefru.h	Snefru/Snefru.h
 git add .

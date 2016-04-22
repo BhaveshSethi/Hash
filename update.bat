3296cea23849f5b21698a73cfb509f50d93fed31
BhaveshSethi <bhaveshsethi1611@gmail.com>
2016-04-23 03:31:50 +0530

    Ripe MD File done

--- update.bat
+++ update.bat
@@ -6,6 +6,7 @@ cp ../MD5_File.cpp		MD5/MD5_File.cpp
 cp ../SHA2.cpp			SHA2/SHA2.cpp
 cp ../SHA2_F.cpp		SHA2/SHA2_F.cpp
 cp ../RIPE_MD.cpp		RIPE-MD/RIPE_MD.cpp
+cp ../RIPEMD_F.cpp		RIPE-MD/RIPEMD_F.cpp
 
 cp ../../INCLUDE/Snefru.h	Snefru/Snefru.h
 git add .
